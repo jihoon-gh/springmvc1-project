2c14b83068d9cce531870f9abb8864d11ee7731c
jihoon-gh <jihoon806@gmail.com>
2023-03-19 22:54:06 +0900

    [FEAT] inflean Spring MVC 1 - done

---
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,42 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+    private static final Map<Long, Item> store = new HashMap<>(); // 멀티스레드 환경에서는 해시맵 쓰면 안됨! 컨커런트해시맵 사용
+    private static long sequence = 0L;
+
+    public Item save(Item item){
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id){
+        return store.get(id);
+    }
+
+    public List<Item> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam){
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+        //중복과 명확성중에는 명확성을 따르자
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+
+
+}
